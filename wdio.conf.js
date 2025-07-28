603ca5add6800bc2ca97f84012323bda72a385b8
Merin P Reji <merin.reji@testingmavens.com>
2025-07-28 14:58:50 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,7 +50,13 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: [
+            '--headless',
+            '--disable-gpu',
+            ]
+        }
     }, 
     // {
     //     browserName: 'MicrosoftEdge'
